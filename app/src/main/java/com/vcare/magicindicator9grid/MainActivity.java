1280a155819046ba8b660bce10b8bf72fc854d35
E_wsq <E_wsq@msn.com>
2021-02-17 23:01:10 +0800

    初始化

---
+++ app/src/main/java/com/vcare/magicindicator9grid/MainActivity.java
@@ -0,0 +1,51 @@
+package com.vcare.magicindicator9grid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.vcare.magicindicator9grid.adapter.ExamplePagerAdapter;
+import com.vcare.magicindicator9grid.ext.navigator.ScaleCircleNavigator;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String[] CHANNELS = new String[]{"CUPCAKE", "DONUT", "ECLAIR", "GINGERBREAD", "HONEYCOMB", "ICE_CREAM_SANDWICH", "JELLY_BEAN", "KITKAT", "LOLLIPOP", "M", "NOUGAT"};
+    private List<String> mDataList = Arrays.asList(CHANNELS);
+    private ExamplePagerAdapter mExamplePagerAdapter = new ExamplePagerAdapter(mDataList);
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mViewPager.setAdapter(mExamplePagerAdapter);
+
+        initMagicIndicator3();
+    }
+
+    private void initMagicIndicator3() {
+        MagicIndicator magicIndicator = (MagicIndicator) findViewById(R.id.magic_indicator3);
+        ScaleCircleNavigator scaleCircleNavigator = new ScaleCircleNavigator(this);
+        scaleCircleNavigator.setCircleCount(CHANNELS.length);
+        scaleCircleNavigator.setNormalCircleColor(Color.LTGRAY);
+        scaleCircleNavigator.setSelectedCircleColor(Color.DKGRAY);
+        scaleCircleNavigator.setCircleClickListener(new ScaleCircleNavigator.OnCircleClickListener() {
+            @Override
+            public void onClick(int index) {
+                mViewPager.setCurrentItem(index);
+            }
+        });
+        magicIndicator.setNavigator(scaleCircleNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+}
